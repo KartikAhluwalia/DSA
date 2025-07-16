2e139a24f29f8ee0949853e3f97f256892ac512f
Kartik Ahluwalia <kartikahluwalia1304@gmail.com>
2025-07-16 10:59:08 +0530

    Updated Bit Manipulation

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -1,6 +1,17 @@
 #To get bit in ith position
 def getBit(num,i):
-    return ((num & (1<<i)) != 0)
+    return int((num & (1<<i)) != 0)
 
+#To set a bit at ith position
 def setBit(num,i):
     return num | (1<<i)
+
+#Clear bit at ith positon
+def clearBit(num,i):
+    mask = ~(1<<i)
+    return num & mask
+
+N = 70
+first = print(getBit(N,1))
+second = print(setBit(N,3))
+third = print(clearBit(N,2))
